87d137406938684140f6dab52c091d42c8d67b90
fahd412 <fahdjaved@hotmail.co.uk>
2025-02-22 23:26:39 +0000

    Book Therapy.php

--- PHP/Book Therapy.php
+++ PHP/Book Therapy.php
@@ -40,7 +40,7 @@ include 'headerlogic.php';
     <title>Book a Session</title>
     <link rel="stylesheet" href="../CSS/booking.css"> 
     <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-">
+
 </head>
 <body>
     <div class="Form-Box">
