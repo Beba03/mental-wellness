1e5dd36be888407434f1326da8280f514d8d4391
omar <f.elsharoud@gmail.com>
2025-03-04 18:55:01 +0000

    Merge pull request #1 from Beba03/Landing-UI
    Landing Page

--- PHP/index.php
+++ PHP/index.php
@@ -66,7 +66,7 @@ if (isset($_GET['action'])) {
         } else {
             $message = implode("<br>", $errors);
         }
-        include 'Forgetpassword.php';
+        include 'Changepassword.php';
     }
 
     /*Log out Logic*/
